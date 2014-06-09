77518114c4d5961d1f6bd643768eabcb54acbe3d
agusyusida@gmail <agusyusida@gmail.com>
2014-06-09 14:10:43 +0700

    a

--- src/routes.php
+++ src/routes.php
@@ -2,33 +2,43 @@
 
 Route::group(array('prefix' => 'mycheckout','before'=>'subdomain'), function()
 {
-	Route::get('','Bcscoder\Jcheckout\JcheckoutMainController@getIndex');
-	Route::post('pengiriman','Bcscoder\Jcheckout\JcheckoutMainController@getPengiriman');
-	Route::get('pengiriman','Bcscoder\Jcheckout\JcheckoutMainController@getPengiriman');
-	Route::get('pembayaran','Bcscoder\Jcheckout\JcheckoutMainController@getPembayaran');
-	Route::post('pembayaran','Bcscoder\Jcheckout\JcheckoutMainController@getPembayaran');
-	Route::get('konfirmasi','Bcscoder\Jcheckout\JcheckoutMainController@getKonfirmasi');
-	Route::post('konfirmasi','Bcscoder\Jcheckout\JcheckoutMainController@getKonfirmasi');
-	Route::post('finish','Bcscoder\Jcheckout\JcheckoutMainController@getFinish');
-	Route::get('finish','Bcscoder\Jcheckout\JcheckoutMainController@getFinish');
+	Route::group(array('before'=>'subdomain'), function()
+
+	{
+		Route::get('','Bcscoder\Jcheckout\JcheckoutMainController@getIndex');
+		Route::post('pengiriman','Bcscoder\Jcheckout\JcheckoutMainController@getPengiriman');
+		Route::get('pengiriman','Bcscoder\Jcheckout\JcheckoutMainController@getPengiriman');
+		Route::get('pembayaran','Bcscoder\Jcheckout\JcheckoutMainController@getPembayaran');
+		Route::post('pembayaran','Bcscoder\Jcheckout\JcheckoutMainController@getPembayaran');
+		Route::get('konfirmasi','Bcscoder\Jcheckout\JcheckoutMainController@getKonfirmasi');
+		Route::post('konfirmasi','Bcscoder\Jcheckout\JcheckoutMainController@getKonfirmasi');
+		Route::post('finish','Bcscoder\Jcheckout\JcheckoutMainController@getFinish');
+		Route::get('finish','Bcscoder\Jcheckout\JcheckoutMainController@getFinish');
 
 
-	Route::get('provinsi/{id}','Bcscoder\Jcheckout\JcheckoutCartController@getProvinsi');
-	Route::get('kabupaten/{id}','Bcscoder\Jcheckout\JcheckoutCartController@getKabupaten');
-	Route::resource('/cart','Bcscoder\Jcheckout\JcheckoutCartController');
-	Route::get('cart/checkdiskon/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@checkdiskon');
-	Route::get('cart/checkekspedisi/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@checkekspedisi');
-	Route::get('cart/addekspedisi/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@addekspedisi');
-	Route::get('cart/delete/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@delete');
+		Route::get('provinsi/{id}','Bcscoder\Jcheckout\JcheckoutCartController@getProvinsi');
+		Route::get('kabupaten/{id}','Bcscoder\Jcheckout\JcheckoutCartController@getKabupaten');
+		Route::resource('/cart','Bcscoder\Jcheckout\JcheckoutCartController');
+		Route::get('cart/checkdiskon/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@checkdiskon');
+		Route::get('cart/checkekspedisi/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@checkekspedisi');
+		Route::get('cart/addekspedisi/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@addekspedisi');
+		Route::get('cart/delete/{any}', 'Bcscoder\Jcheckout\JcheckoutCartController@delete');
 
+		Route::group(array('prefix' => 'finpay'), function()
+		{
+			Route::get('/','Bcscoder\Jcheckout\JcheckoutMainController@finpayDemo');
+			Route::post('/create','Bcscoder\Jcheckout\JcheckoutMainController@createFinPay');
+			Route::post('/check','Bcscoder\Jcheckout\JcheckoutMainController@checkFinPay');
+			Route::post('/cancel','Bcscoder\Jcheckout\JcheckoutMainController@finpayDemo');
+
+		});
+	});
 	Route::group(array('prefix' => 'finpay'), function()
 	{
-		Route::get('finpay','Bcscoder\Jcheckout\JcheckoutMainController@getFinPayResponse');
-		Route::get('/','Bcscoder\Jcheckout\JcheckoutMainController@finpayDemo');
-		Route::post('/create','Bcscoder\Jcheckout\JcheckoutMainController@createFinPay');
-		Route::post('/check','Bcscoder\Jcheckout\JcheckoutMainController@checkFinPay');
-		Route::post('/cancel','Bcscoder\Jcheckout\JcheckoutMainController@cancelFinPay');
+		Route::get('response','Bcscoder\Jcheckout\JcheckoutMainController@getFinPayResponse');
 	});
+	
+	
 	
 
 	Route::get('tes',function(){
